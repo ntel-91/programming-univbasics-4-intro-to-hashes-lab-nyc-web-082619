13e13a9cede3b4b7549beac39ce1bf40902ae7db
Nick Telenson <ntelenson@gmail.com>
2019-08-09 13:27:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,12 @@
 def new_hash
   # return an empty hash
+  new = {}
+  return new 
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  new = { :weekdays => "Tuesday" }
 end
 
 def pioneer
